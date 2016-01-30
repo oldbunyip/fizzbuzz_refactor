1a1124957feec7c7877a492e3ab24245c81e8f83
oldbunyip <oldbunyip@gmail.com>
2016-01-30 18:00:30 +1300

    second commit

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,6 +1,6 @@
 var number = prompt("Please enter a number between 1 and 100");
 
-for (var i=1; i<=100; i++){
+for (var i=1; i<=number; i++){
         if(i%5 === 0 && i%3 === 0){
             document.write("<p>FizzBuzz</p>");
         } 
